49c82d489455c0bce3fc095229703593cb4ecdf9
Gheribi Abd-el-rahmen <117765552+TawahinBeirut@users.noreply.github.com>
2023-10-28 10:26:11 +0200

    Ajoute du about me et du footer

--- src/Components/Rubrics/PresentationR.js
+++ src/Components/Rubrics/PresentationR.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import Themes from '../../Themes.json'
 
 export default function PresentationR() {
   return (
-    <div>PresentationR</div>
+    <div className='flex justify-center mt-20'>
+      <div className='w-4/6 flex text-center'>
+      <div className=" bg-gray-200 h-full w-1"></div>
+      <div>
+      <h1 className='font-bold'>{Themes.Presentation.Title}</h1>
+      <div className='text-center font-Presentation mt-2 flex flex-col gap-10'>
+        {Themes.Presentation.Text.map(el => (
+          <p>{el}</p>
+        ))}
+      </div>
+      </div>
+      </div>
+    </div>
   )
 }
