8160c839068565e268e56db89395117821b236ba
tawahinBeirut <gheribij59@gmail.com>
2023-11-01 10:59:51 +0100

    Responsive finished

--- src/Components/Presentation.js
+++ src/Components/Presentation.js
@@ -6,7 +6,7 @@ import { delay, motion } from "framer-motion"
 export default function Presentation() {
   return (
     <div className="h-screen overflow-hidden">
-        <div className='h-5/6 bg-gradient-to-tr from-Abd-el-Bg1 to-Abd-el-Bg2 flex drop-shadow-2xl overflow-hidden justify-stretch '>
+        <div className='h-4/6 bg-gradient-to-tr from-Abd-el-Bg1 to-Abd-el-Bg2 flex drop-shadow-2xl overflow-hidden justify-stretch sm:h-5/6'>
             <motion.h1 initial={{opacity: 0,y : 10}} animate={{opacity: 1 , y: 0}} transition={{delay: 1}} className="font-Button text-xl ml-5">Portfolio</motion.h1>
             <motion.img initial={{opacity: 0, x : -10}} animate={{opacity: 1,x : 0}} transition={{delay: 0.5}} src={img} className=' hidden -ml-10 sm:flex sm:w-fit'></motion.img>
             <div className='flex flex-col w-4/6 justify-start mt-7'>
