c1274a9a93b1437ac4a54917ba12c46c501805ac
tawahinBeirut <gheribij59@gmail.com>
2023-11-02 01:07:11 +0100

    Last Modification

--- src/Components/Presentation.js
+++ src/Components/Presentation.js
@@ -6,20 +6,21 @@ import { delay, motion } from "framer-motion"
 export default function Presentation() {
   return (
     <div className="h-screen overflow-hidden">
-        <div className='h-4/6 bg-gradient-to-tr from-Abd-el-Bg1 to-Abd-el-Bg2 flex drop-shadow-2xl overflow-hidden justify-stretch sm:h-5/6'>
-            <motion.h1 initial={{opacity: 0,y : 10}} animate={{opacity: 1 , y: 0}} transition={{delay: 1}} className="font-Button text-xl ml-5">Portfolio</motion.h1>
+        <div className='h-4/6 bg-gradient-to-tr from-Abd-el-Bg1 to-Abd-el-Bg2 sm:flex drop-shadow-2xl overflow-hidden sm:justify-stretch sm:h-5/6'>
+            <div className='flex justify-center'><motion.h1 initial={{opacity: 0,y : 10}} animate={{opacity: 1 , y: 0}} transition={{delay: 1}} className="font-Button text-xl ml-5">Portfolio</motion.h1></div>
             <motion.img initial={{opacity: 0, x : -10}} animate={{opacity: 1,x : 0}} transition={{delay: 0.5}} src={img} className=' hidden -ml-10 sm:flex sm:w-fit'></motion.img>
-            <div className='flex flex-col w-4/6 justify-start mt-7'>
+            <div className='flex justify-center sm:justify-end'><div className='flex flex-col w-4/6 justify-start mt-7'>
                 <div className="flex justify-center"><h1 className='text-center text-2xl tablet:text-6xl '>{Themes.General.Profession}</h1></div>
                 <div className='flex justify-center'><h1 className=' mt-5 font-thin text-center text-xl tablet:text-3xl'>{Themes.General.Prenom1} {Themes.General.Prenom2}</h1></div>
                 <div className='flex justify-center'><h1 className='font-thin text-xl tablet:text-3xl'>{Themes.General.Nom}</h1></div>
-                <div className='flex text-2xl font-thin justify-evenly mt-5 text-center'>
+                <div className='flex text-2xl font-thin justify-evenly mt-5 text-center gap-3'>
                     {Themes.General.Qualitites.map(el => (
                         <p>{el}</p>
                     ))}
                 </div>
                 <div className='flex justify-center'><div className='bg-black rounded-xl h-1 w-4/6 mt-5'></div></div>
                 <div className='flex justify-center'><div className='w-3/6 text-center font-thin mt-5 text-xl'>{Themes.General.ShortPresentation}</div></div>
+            </div>
             </div>
         </div>
     </div>
--- src/Components/Rubrics/Project.js
+++ src/Components/Rubrics/Project.js
@@ -9,8 +9,8 @@ export default function Project({title,image,description,Link,GitLink}) {
   return (
     <div className=' drop-shadow-2xl flex flex-col max-w-lg rounded-xl overflow-hidden mt-10 '>
         <img src={image}/>
-        <div className='flex flex-col gap-5 bg-white text-lg sm:text-xl text-center p-5'> 
-        <div className='font-bold font-Presentation'><h1>{title}</h1></div>
+        <div className='flex flex-col gap-5 bg-white text-sm sm:text-xl text-center p-5'> 
+        <div className='font-bold font-Presentation text-xl'><h1>{title}</h1></div>
         <div className='flex justify-center'><div className='bg-black rounded-xl h-1 w-4/6'></div></div>
         <div className='font-Presentation font-light mt-4'><p>{description}</p></div>
         {Link ? <div className='font-Presentation text-blue-300'><a href={Link}>{Link}</a></div> : null}
