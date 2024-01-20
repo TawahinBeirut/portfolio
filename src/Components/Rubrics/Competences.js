c02082dde422ae66a12950eaffff20ca32374aed
tawahinBeirut <gheribij59@gmail.com>
2024-01-20 22:17:19 +0100

    push

--- src/Components/Rubrics/Competences.js
+++ src/Components/Rubrics/Competences.js
@@ -1,39 +1,34 @@
-import React,{useEffect} from "react";
+import React, { useEffect } from 'react'
 import Themes from "../../Themes.json"
+import Project from './Project';
 
-import {motion,useAnimation} from 'framer-motion'
+import img from '../../Assets/FinalDesign.png';
 
-import { useInView } from 'react-intersection-observer';
+import img1 from '../../Assets/Languages.png';  
+import img2 from '../../Assets/framework1.png';
+import img3 from '../../Assets/English.png';
+import img4 from '../../Assets/Work_Team.png'  
+import img5 from '../../Assets/Figma.png' 
+import {motion} from 'framer-motion'
 
-const RubricsVariants = {
-    visible : {opacity : 1, x : 0, transition: {duration: 0.5}},
-    hidden : {opacity: 0, x : -10}
-  };
+export default function Projects() {
+  const tab = [img1,img2,img3,img4,img5];
 
-export default function Competences(){
-
-    
-  const controls = useAnimation();
-  const [ref,inView] = useInView();
-
-  useEffect(() => {
-    if (inView){
-      controls.start("visible")
-    }
-  },[controls,inView])
-
-    return (
-        <div className='flex justify-center mt-20 drop-shadow-xl'>
-      <div className='w-4/6 flex text-center'>
-      <div>
-      <h1 className='font-bold font-Presentation text-3xl'>{Themes.Competences.Title}</h1>
-      <div className='text-center text-xl font-Presentation mt-2 flex flex-col gap-10 font-bold'>
-        {Themes.Competences.Text.map(el => (
-          <motion.div ref={ref} animate={controls} variants={RubricsVariants} initial="hidden">{el}</motion.div>
-        ))}
-      </div>
+  let i = -1 ;
+  
+  return (
+    <div className='flex justify-center mt-20'>
+      <div className='flex flex-col'>
+      <div className='text-center font-bold font-Presentation text-3xl'>Projects</div>
+      <div className="flex flex-wrap justify-center gap-12">
+        
+        {Themes.Competences.map(el => {
+          i++;
+          return (
+          <motion.div whileHover= {{scale: 1.1, transition:{duration :1}}}><Project  title={el.Title} image={tab[i] ? tab[i] : img } description={el.Description}/></motion.div>)
+;})}
       </div>
       </div>
     </div>
-    )
+  )
 }
