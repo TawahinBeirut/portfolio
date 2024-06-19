314e4a989c08cd0f357ee28bcc0f67e5221299b4
Jasser <117765552+TawahinBeirut@users.noreply.github.com>
2024-06-20 01:13:38 +0200

    qsdqs

--- src/Components/Rubrics.js
+++ src/Components/Rubrics.js
@@ -6,23 +6,33 @@ import Projects from "./Rubrics/Projects";
 import Contact from "./Rubrics/Contact";
 import Footer from "./Footer";
 import Competences from "./Rubrics/Competences";
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 export default function Rubrics() {
   const [Selected, setSelected] = useState(Themes.General.Rubrics[0]);
 
 
-  
+
   const onClick = (e) => {
     setSelected(e.target.name);
   };
 
   return (
-      <div className="flex flex-col -mt-40 sm:-mt-32 ">
-        <PresentationR />
-        <Projects/>
-        <Competences/>
-        <Contact/>
-        <Footer/>
-      </div>
+    <div className="flex flex-col -mt-40 sm:-mt-32 ">
+      <Tabs>
+        <TabList>
+          <Tab>Presentation</Tab>
+          <Tab>Projets</Tab>
+          <Tab>Competences</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel> <PresentationR /></TabPanel>
+
+          <TabPanel><Projects /></TabPanel>
+          <TabPanel><Competences /></TabPanel>
+        </TabPanels>
+      </Tabs>
+      <Contact />
+      <Footer />
+    </div>
   );
 }
